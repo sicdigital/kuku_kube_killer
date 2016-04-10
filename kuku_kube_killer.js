2cab69cb6bbf724f4f5954bb30f350454f43613a
Mike Chacon <mike@sicdigital.com>
2016-04-11 04:36:56 +0700

    Updates the script to fix a bug where it wouldnt execute if the cube was the last one, got around it by just clicking both cubes I'm storing in the array

--- kuku_kube_killer.js
+++ kuku_kube_killer.js
@@ -7,13 +7,13 @@ var color_array = [];
 var unique;
 
 //Loop through all of the boxes
-for( i = 0; i < boxes.length + 1; i++)
+for( i = 0; i < boxes.length; i++)
  {
  	//Assigns the current item's color to the current_box_color variable
  	var current_box_color = boxes[i].style.backgroundColor;
 
-		// Checks the current items color against the first and second values in color_array.
-		if(color_array[0] !== current_box_color  && color_array[1] !== current_box_color ){
+		// Checks the current items color against the first and second values in color_array. It must be unique to both
+		if(color_array[0] !== current_box_color && color_array[1] !== current_box_color ){
 
 			//If it doesn't match both of these add it to the color_array, the first time around it will fill this array
 			color_array.push(current_box_color);
@@ -21,30 +21,7 @@ for( i = 0; i < boxes.length + 1; i++)
 			//Store this box in the box_array
 			box_array.push(boxes[i]);
 	 }
-
- 	//If a box has been pushed to the first position of the box array
-	if(box_array[0]){
-
-		//Check if box_array[0] matches the current boxes background color, if it does, then it isn't unique
-		if(box_array[0].style.backgroundColor == boxes[i].style.backgroundColor){
-
-			// If it doesn't then put it in box_array[1], we now have 2 colors stored which are different
-			unique = box_array[1];
-		}
-	}
-
-	 //If there is a value in box_array[1], the loop has looped at least 2x
-	if(box_array[1]){
-
-		// If box_array[1] background color matches the current iteration it isn't unique
-		if(box_array[1].style.backgroundColor == boxes[i].style.backgroundColor){
-
-			// Since box_array[1] is not unique, the value in box_array[0] must be
-			unique = box_array[0] ;
-		}
-	}
 }
-	console.log(unique.style.backgroundColor);
-	unique.style.backgroundColor == "#fff";
-	unique.click();
+	box_array[0].click();
+  box_array[1].click();
 }, 10);
